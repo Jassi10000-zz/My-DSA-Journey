7a893bdf58599a7374f060fe41236c75d2102c03
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-02 12:17:16 +0530

    commit

--- L003/reverseANo.java
+++ L003/reverseANo.java
@@ -1,3 +1,6 @@
+1. This is the first presentation of a reverse no.
+I/P : 8366435     O/P : 5346638
+    
 import java.util.Scanner;
 
 public class reverseANo {
@@ -21,6 +24,36 @@ public class reverseANo {
         int n = sc.nextInt();
     
         System.out.println("Reverse is : "+ reverseANo_(n));
+    
+    }
+
+}
+
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void reverseANo_(int n) {
+
+        
+        while (n > 0) {
+
+            int r = n % 10;
+            System.out.println(r);
+            n = n / 10;
+        }
+        
+    }
+
+    public static void main(String []args){
+    
+        int n = sc.nextInt();
+    
+        reverseANo_(n);
     
     }
 
