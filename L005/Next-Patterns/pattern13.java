90ebf7485106a06906052031be65a1cdd81284b9
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-12 21:19:22 +0530

    new update

--- L005/Next-Patterns/pattern13.java
+++ L005/Next-Patterns/pattern13.java
@@ -6,10 +6,17 @@ public class pattern13 {
 
     public static void pattern13_(int n) {
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                int a = (int)Math.pow(11,j);
-                System.out.print(a + "\t");
+        for (int i = 0; i < n; i++) {
+
+            int icj = 1;
+
+            for (int j = 0; j <= i; j++) {
+
+                System.out.print(icj + "\t");
+
+                int icjp1 = icj * (i - j) / (j + 1);
+
+                icj = icjp1;
             }
             System.out.println();
         }
