74828bc094cbe873b7af42dbe148b0088f18af72
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-12 17:27:18 +0530

    new update

--- L005/Next-Patterns/pattern12.java
+++ L005/Next-Patterns/pattern12.java
@@ -1,26 +1,28 @@
-#include<iostream>
-using namespace std;
+import java.util.Scanner;
 
+public class pattern12 {
 
-void pattern12_(int n){
-     int a=0 , b=1;
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void pattern12_(int n) {
+
+        int a=0 , b=1;
 
         for (int i = 1; i <= n; i++) {
             for (int j= 1; j <=i; j++) {
-                cout<<a<<"\t";
+                System.out.print(a + "\t");
                 int c = a + b;
                 a = b;
                 b = c;
             }
-            cout<<endl;
+            System.out.println();
         }
+
+    }
+
+    public static void main(String[] args) {
+
+        int n = sc.nextInt();
+        pattern12_(n);
+    }
 }
-
-int main(){
-
-    int n;
-    cin>>n;
-    pattern12_(n);
-
-    return 0;
-}                                     
