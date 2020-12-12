2b03c3361734b50c283b802aeff874bab9c9692b
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-12 13:58:30 +0530

    updated

--- L005/Next-Patterns/pattern16.cpp
+++ L005/Next-Patterns/pattern16.cpp
@@ -4,17 +4,34 @@ using namespace std;
 
 void pattern16(int n){
 
-    int st=1;
-    int count = 0;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=st;j++){
-            count++;
-            cout<<count + "\t";
-            
+    int sp = 2 * n - 3;
+        int st = 1;
+
+        for (int i = 1; i <= n; i++) {
+            int val = 1;
+            for (int j = 1; j <= st; j++) {
+                cout<<val<<"\t";
+                val++;
+            }
+
+            for (int j = 1; j <= sp; j++) {
+                cout<<"\t";
+            }
+
+            if (i == n) {
+                st--;
+                val--;
+            }
+
+            for (int j = 1; j <= st; j++) {
+                val--;
+                cout<<val<<"\t";
+            }
+
+            st++;
+            sp -= 2;
+            cout<<endl;
         }
-        st++;
-        cout<<endl;
-    }
 
 }
 int main(){
