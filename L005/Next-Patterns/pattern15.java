e593cbe1d1b9bce45ed1a6d872ae862301ace16d
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-12 17:06:34 +0530

    updated

--- L005/Next-Patterns/pattern15.java
+++ L005/Next-Patterns/pattern15.java
@@ -1,1 +1,55 @@
-dadcX
+import java.util.Scanner;
+
+public class pattern15 {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void pattern15_(int n) {
+
+        int sp = n / 2;
+        int st = 1;
+        int val = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sp; j++) {
+                System.out.print("\t");
+            }
+
+            int cval = val;
+        for(int j = 1 ; j<=st;j++){
+            System.out.print(cval + "\t");
+            
+
+            if(j <= st/2){
+                cval++;
+            }
+            else{
+                cval--;
+            }
+        }
+
+           
+
+        if(i <= n/2){
+
+
+            sp--;
+            st += 2;
+            val++;
+        }
+        else{
+            sp++;
+            st -= 2;
+            val--;
+        }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int n = sc.nextInt();
+
+        pattern15_(n);
+
+    }
+}
