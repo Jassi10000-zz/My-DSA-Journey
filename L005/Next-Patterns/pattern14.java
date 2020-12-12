e29aba9c7af2304286f53727c0d9595fdd0ba25c
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-12 17:17:52 +0530

    updated

--- L005/Next-Patterns/pattern14.java
+++ L005/Next-Patterns/pattern14.java
@@ -1,1 +1,22 @@
-sfsf
+import java.util.Scanner;
+
+public class pattern14 {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void pattern14_(int x) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(x + " * " + i + " = " + x * i);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int n = sc.nextInt();
+
+        pattern14_(n);
+
+    }
+}
