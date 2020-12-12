787ddbd79bbfedfc8a0dc4e71fdb07f51dcd6f19
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-12 19:17:21 +0530

    updated code

--- L005/Next-Patterns/pattern10.java
+++ L005/Next-Patterns/pattern10.java
@@ -1,6 +1,56 @@
 import java.util.Scanner;
 
-public class pattern10{
+public class pattern10 {
 
-    
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void pattern10_(int n) {
+
+        int outSp = n / 2;
+
+        int inSp = -1;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= outSp; j++) {
+                System.out.print("\t");
+            }
+
+            System.out.print("*\t");
+
+            for (int j = 1; j <= inSp; j++) {
+                System.out.print("\t");
+            }
+
+            if (i > 1 && i < n) {
+                System.out.print("*\t");
+            }
+
+            if (i <= n / 2) {
+
+                outSp--;
+
+                inSp += 2;
+            }
+
+            else {
+
+                outSp++;
+
+                inSp -= 2;
+
+            }
+
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String []args){
+
+        int n = sc.nextInt();
+
+        pattern10_(n);
+    }
+
 }
