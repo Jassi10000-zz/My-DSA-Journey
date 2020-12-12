219a4b347cc172a344f188900f7fdd48cd41708d
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-12 17:18:15 +0530

    update

--- L005/Next-Patterns/pattern14.cpp
+++ L005/Next-Patterns/pattern14.cpp
@@ -1,1 +1,20 @@
-sdsd
+#include<iostream>
+using namespace std;
+
+
+void pattern14_(int x){
+    
+    for(int i=1;i<=10;i++){
+        cout<<x<<"*"<<i<<"="<<x*i<<endl;
+    }
+    
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    pattern14_(n);
+    return 0;
+}
