dee4a3367e4e184b3080e27fd752b89445882b2e
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-12 17:27:46 +0530

    new update

--- L005/Next-Patterns/pattern12.cpp
+++ L005/Next-Patterns/pattern12.cpp
@@ -1,1 +1,26 @@
-sdfsf
+#include<iostream>
+using namespace std;
+
+
+void pattern12_(int n){
+     int a=0 , b=1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j= 1; j <=i; j++) {
+                cout<<a<<"\t";
+                int c = a + b;
+                a = b;
+                b = c;
+            }
+            cout<<endl;
+        }
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+    pattern12_(n);
+
+    return 0;
+}                                     
