d4c04b0ecc0b93e33acb88893332244c4a1b216f
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-12 21:18:50 +0530

    update

--- L005/Next-Patterns/pattern13.cpp
+++ L005/Next-Patterns/pattern13.cpp
@@ -1,1 +1,32 @@
-fes
+#include<iostream>
+using namespace std;
+
+
+void pattern13(int n){
+
+    for(int i=0;i<n;i++){
+
+        int icj = 1;
+        for(int j=0;j<=i;j++){
+
+            cout<<icj<<"\t";
+
+            int icjp1 = icj * (i - j ) / (j + 1);
+            
+            icj = icjp1;
+            
+        }
+
+        cout<<endl;
+    }
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+
+
+    pattern13(n);
+    return 0;
+}
