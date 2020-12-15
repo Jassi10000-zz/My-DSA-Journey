37fd19c896acf44d4f7bab6160d87f4434bcb1a5
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-15 12:05:51 +0530

    updated code

--- L006/Arrays/firstIdx.java
+++ L006/Arrays/firstIdx.java
@@ -1,1 +1,37 @@
-lndslkslkfn
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class firstIdx{
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static int firstIdx_(int[] arr, int target){
+        
+        int idx = -1;
+
+
+        for(int i=0; i<arr.length;i++){
+            if ( arr[i] == target) {
+                idx= i;
+                break;
+            }
+        }
+
+
+        return idx;
+    }   
+
+    public static void main(String[] args) {
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int d = sc.nextInt();
+        System.out.println(firstIdx_(arr, d));
+
+    }
+}
