6c3ecf378e13040cc1a657952b282d5c5fb6af32
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-15 12:02:41 +0530

    update

--- L006/Arrays/firstIdx.cpp
+++ L006/Arrays/firstIdx.cpp
@@ -1,8 +1,13 @@
-private static int firstIndex(int[] arr, int target){
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int firstIdx( vector<int>arr, int target){
         
         int idx = -1;
 
-        for(int i=0; i < arr.length; i++){
+
+        for(int i=0; i<arr.size();i++){
             if ( arr[i] == target) {
                 idx= i;
                 break;
@@ -12,3 +17,18 @@ private static int firstIndex(int[] arr, int target){
 
         return idx;
     }   
+
+int main(){
+
+    int n,d;
+    cin>>n;
+
+    vector<int>arr(n,0);
+    for(int i=0;i<arr.size();i++){
+        cin>>arr[i];
+    }
+
+    cin>>d;
+    cout<<"First index of "<<d<<" is "<<firstIdx(arr,d);
+    return 0;
+}
