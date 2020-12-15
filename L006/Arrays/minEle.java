e76aa0480b0f44217477417a2016d8401e091963
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-15 11:59:23 +0530

    updated

--- L006/Arrays/minEle.java
+++ L006/Arrays/minEle.java
@@ -1,1 +1,28 @@
-rgfs
+import java.util.Scanner;
+
+public class minEle{
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static int minEle_(int[] arr){
+
+        int minElem = (int) 1e8;
+        for(int ele:arr){
+            if(ele < minElem) minElem = ele;
+        }
+        return minElem;
+    }
+
+    public static void main(String[] args){
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Min element is : " + minEle_(arr));
+    }
+}
