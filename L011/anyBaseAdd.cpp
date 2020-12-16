6d3dc94a63767c469f10c235d237eef5a5e91e9e
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-16 22:59:21 +0530

    update

--- L011/anyBaseAdd.cpp
+++ L011/anyBaseAdd.cpp
@@ -1,8 +1,36 @@
 #include<iostream>
 using namespace std;
 
-int main(){
 
+int getSum(int b, int n1, int n2){
+       // write ur code here
+       int ans=0,carry=0,p=1;
+       while(n1!=0||n2!=0||carry!=0){
+           int r1=n1%10;
+           int r2=n2%10;
+           
+           n1/=10;
+           n2/=10;
+           
+           int t=r1+r2+carry;
+           carry=t/b;
+           t=t%b;
+           
+           ans=ans+t*p;
+           p=p*10;
+           
+       }
+       return ans;
+   }
 
-    return 0;
-}
+ int main() {
+     
+      int b , n1 , n2;
+      cin>>b>>n1>>n2; 
+  
+      int d = getSum(b, n1, n2);
+      cout<<d;
+      return 0;
+   }
+  
+   
