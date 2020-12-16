2bba94b0aa4ef02e11c67139a9acc1651cab3db5
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-16 21:13:49 +0530

    updated

--- L006/Arrays/lastIdx.cpp
+++ L006/Arrays/lastIdx.cpp
@@ -1,13 +1,31 @@
-private static int lastIndex(int[] arr, int target){
-        int idx = -1;
+#include<iostream>
+#include<vector>
+using namespace std;
 
-        for(int i=arr.length - 1; i  >= 0; i--){
+int lastIdx( vector<int>arr, int target){
+        
+        int idx = -1;
+        for(int i=arr.size()-1; i>=0;i--){
             if ( arr[i] == target) {
                 idx= i;
                 break;
             }
         }
 
-
         return idx;
+    }   
+
+int main(){
+
+    int n,d;
+    cin>>n;
+
+    vector<int>arr(n,0);
+    for(int i=0;i<arr.size();i++){
+        cin>>arr[i];
     }
+
+    cin>>d;
+    cout<<"Last index of "<<d<<" is "<<lastIdx(arr,d);
+    return 0;
+}
