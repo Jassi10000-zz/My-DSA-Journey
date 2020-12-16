7b7823487a78c6903c7586f6912f9d7b9867a63f
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-16 19:25:06 +0530

    commit

---
+++ L012/stringCompression.cpp
@@ -0,0 +1,18 @@
+// what we have to achieve
+// abaabbbccddeffghhh ------->  a3b1a2b3c2d2e1f2g1h3 --------> a3b4c2d2e1f2g1h3
+
+#include<iostream>
+using namespace std;
+
+int strCom(){
+
+
+}
+
+int main(){
+
+    string str;
+    cin>>str;
+
+    return 0;
+}
---
+++ L012/stringCompression.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class stringCompression{
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static int strCom(){
+
+
+    }
+
+    public static void main(String[] args){
+
+        String str = sc.nextLine();
+
+        strCom(str);
+    }
+}
