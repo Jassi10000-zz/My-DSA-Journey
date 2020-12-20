78e102bf92f3a34ca18c2007cb5be89abeed39b0
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-20 23:40:15 +0530

    commit

--- L010/saddlePoint.java
+++ L010/saddlePoint.java
@@ -1,1 +1,51 @@
-sdfs
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static Scanner sc = new Scanner(System.in);
+    
+    
+    public static void saddlePoint(int[][] arr){
+        
+        for(int i=0;i<arr.length;i++){
+            int svj =0;
+            
+            for(int j=0;j<arr[0].length;j++){
+                if(arr[i][j] < arr[i][svj]) svj =j;
+            }
+        
+        
+            boolean flag= true;
+            for(int k=0;k<arr.length;k++){
+                if(arr[k][svj] > arr[i][svj]){
+                    flag = false;
+                    break;
+                }
+            }
+        
+            if(flag == true){
+                System.out.println(arr[i][svg]);
+                return;
+            }
+    }
+    System.out.println("Invalid input");
+    }    
+
+    public static void main(String[] args) throws Exception{
+        // write your code here
+        
+        int n = sc.nextInt();
+        
+        int[][]arr = new int[n][n];
+        
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        
+        saddlePoint(arr);
+        
+    }
+}
