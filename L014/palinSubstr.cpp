689ba9fd5ac740c5cd581a1348164d0eb94da23b
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-24 15:41:00 +0530

    update

--- L014/palinSubstr.cpp
+++ L014/palinSubstr.cpp
@@ -1,1 +1,38 @@
-EHRGD
+#include<iostream>
+using namespace std;
+
+bool isPalindrome(string str){
+
+    int i=0;
+    int j=str.length()-1;
+
+    while(i<j){
+
+        if(str[i++] != str[j--]) return false;
+
+    }
+    return true;
+}
+void palinSubstr(string str){
+
+    for(int i=0;i<str.length();i++){
+            for(int len=1;i+len<=str.length();len++){
+                
+                string s = str.substr(i,len);
+                if(isPalindrome(s)){
+                    cout<<s<<endl;
+                }
+            }
+        }
+}
+
+
+
+int main(){
+
+    string str;
+    cin>>str;
+
+    palinSubstr(str);
+    return 0;
+}
