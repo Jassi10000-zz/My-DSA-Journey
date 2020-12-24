b17c3391bd21ec6f14ac6cf072f4764ecd956362
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-24 23:14:49 +0530

    update

--- L014/strCompress.cpp
+++ L014/strCompress.cpp
@@ -1,1 +1,35 @@
-EHHTNN
+#include<iostream>
+using namespace std;
+
+void compression1(string str) {
+	int i;
+	for(i=0; i<str.length()-1; i++) {
+		if(str[i] != str[i+1])
+		cout<<str[i];
+	}
+	cout<<str[str.length()-1];
+}
+void compression2(string str) {
+	int i, count = 1;
+	for(i=0; i<str.length()-1; i++) {
+		if(str[i] == str[i+1])
+		count++;
+		else if(count>1) {
+			cout<<str[i]<<count;
+			count = 1;
+		} 
+		else 
+		cout<<str[i];
+	}
+	cout<<str[str.length()-1];
+	if(count > 1)
+	cout<<count;
+}
+int main() {
+	string str;
+	cin>>str;
+	compression1(str);
+	cout<<"\n";
+	compression2(str);
+	return 0;
+}
