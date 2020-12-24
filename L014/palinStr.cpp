31a93dc0492b8c63d81b74ee357f0cf744f76a9b
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-24 15:05:55 +0530

    update

--- L014/palinStr.cpp
+++ L014/palinStr.cpp
@@ -1,1 +1,24 @@
-WEF
+#include<iostream>
+using namespace std;
+
+bool palinStr(string str){
+
+    int i=0;
+    int j=str.length()-1;
+
+    while(i<j){
+
+        if(str[i++] != str[j--]) return false;
+
+    }
+    return true;
+}
+
+int main(){
+
+    string str;
+    cin>>str;
+
+    cout<<palinStr(str);
+    return 0;
+}
