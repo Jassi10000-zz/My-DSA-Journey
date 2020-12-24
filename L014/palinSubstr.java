6a6451656c178f9f469c2b76f9adcc1940a52d54
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-24 15:41:29 +0530

    update

--- L014/palinSubstr.java
+++ L014/palinSubstr.java
@@ -1,1 +1,40 @@
-DFBDSER
+import java.util.Scanner;
+
+public class palinSubstr{
+
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void palinSubstr_(String str){
+
+        for(int i=0;i<str.length();i++){
+            for(int len=1;i+len<=str.length();len++){
+                String s = str.substring(i,i+len);
+                if(isPalindrome(s)){
+                    System.out.println(s);
+                }
+            }
+        }
+
+    }
+
+    public static boolean isPalindrome(String str){
+        int i=0;
+        int j=str.length()-1;
+
+    while(i<j){
+
+        if(str.charAt(i++) != str.charAt(j--)) return false;
+
+    }
+    return true;
+
+    }
+    
+    public static void main(String[] args){
+
+        String str = sc.nextLine();
+
+        palinSubstr_(str);
+    }
+}
