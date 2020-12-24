ae4280074dcbb9fdc2a9c82a5545529d5c6157a2
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-24 15:05:17 +0530

    update

--- L014/palinStr.java
+++ L014/palinStr.java
@@ -1,1 +1,29 @@
-WEF
+import java.util.Scanner;
+
+public class palinStr{
+
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static boolean palinStr_(String str){
+
+        int i=0;
+        int j=str.length()-1;
+
+    while(i<j){
+
+        if(str.charAt(i++) != str.charAt(j--)) return false;
+
+    }
+    return true;
+
+    }
+    
+    public static void main(String[] args){
+
+        String str = sc.nextLine();
+
+        System.out.println(palinStr_(str));
+        
+    }
+}
