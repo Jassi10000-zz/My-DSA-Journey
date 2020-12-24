fc08c80f06722ac7ab1754dfbfb45f0b65db2b34
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-24 14:47:03 +0530

    update

--- L014/allSubstr.java
+++ L014/allSubstr.java
@@ -1,1 +1,24 @@
-FWE
+import java.util.Scanner;
+
+public class allSubstr{
+
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void allSubstr_(String str){
+
+        for(int i=0;i<str.length();i++){
+            for(int len=1;i+len<=str.length();len++){
+                System.out.println(str.substring(i,i+len));
+            }
+        }
+
+    }
+    
+    public static void main(String[] args){
+
+        String str = sc.nextLine();
+
+        allSubstr_(str);
+    }
+}
