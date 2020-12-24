17f49f69fdb357d86ce311d57eb0eeb128d93308
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-24 14:55:33 +0530

    update

--- L014/allSubstr.cpp
+++ L014/allSubstr.cpp
@@ -1,1 +1,20 @@
-WF
+#include<iostream>
+using namespace std;
+
+void allSubstr(string str){
+
+    for(int i=0;i<str.length();i++){
+            for(int len=1;i+len<=str.length();len++){
+                cout<<str.substr(i,len)<<endl;
+            }
+        }
+}
+
+int main(){
+
+    string str;
+    cin>>str;
+
+    allSubstr(str);
+    return 0;
+}
