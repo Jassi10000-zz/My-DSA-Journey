f95a473a598e5271473c9d8eb32cae95b3c0fb8b
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-25 10:57:33 +0530

    update

--- L014/secverWithoutX2.java
+++ L014/secverWithoutX2.java
@@ -1,3 +1,15 @@
+withoutX2("xHi") → "Hi"
+withoutX2("Hxi") → "Hi"
+withoutX2("Hi") → "Hi"
+withoutX2("xxHi") → "Hi"
+withoutX2("Hix") → "Hi"
+withoutX2("xaxb") → "ab"
+withoutX2("xx") → ""
+withoutX2("x") → ""
+withoutX2("") → ""
+withoutX2("Hello") → "Hello"
+withoutX2("Hexllo") → "Hello"
+withoutX2("xHxllo") → "Hllo"
 import java.util.Scanner;
 
 public class withoutX2 {
