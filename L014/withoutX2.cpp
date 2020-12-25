53051a64ef06186f46799dbdc12f47a2095d9a2e
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-25 11:27:39 +0530

    commit

--- L014/withoutX2.cpp
+++ L014/withoutX2.cpp
@@ -1,1 +1,24 @@
-RGSBGR
+#include<iostream>
+using namespace std;
+
+
+string withoutX2_(string str){
+
+        string st;
+
+        for(int i=0;i<str.length();i++){
+            if(i<2 && str[i] != 'x') st.push_back(str[i]);
+            if(i>=2) st.push_back(str[i]);
+        }
+
+        return st;
+    }
+
+    int main(){
+
+        string str;
+        cin>>str;
+
+        cout<<withoutX2_(str)<<endl;
+        
+    }
