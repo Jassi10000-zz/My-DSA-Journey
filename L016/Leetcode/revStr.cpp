3fc8cd1be5282d6b9ae9ef30a536319e928efd9b
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-28 15:37:18 +0530

    update

--- L016/Leetcode/revStr.cpp
+++ L016/Leetcode/revStr.cpp
@@ -1,3 +1,28 @@
-class{
-
+class Solution {
+public:
+    
+    void reverseRange(string& s,int i, int j){
+        while(i < j){
+            swap(s[i++],s[j--]);
+        }
+    }
+    string reverseStr(string s, int k) {
+        
+        if(k == 0 || k == 1 || s.length() <= 1) return s;
+        
+        int i = 0;
+        int n = s.length();
+        
+        while(i < n){
+            if( i + k - 1 < n){
+                reverseRange(s,i,i+k-1);
+                i += 2*k;
+            }
+            else{
+                reverseRange(s,i,n-1);
+                break;
+            }
+        }
+        return s;
+    }
 };
