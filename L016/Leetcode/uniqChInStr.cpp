b0a30bfcc17912f084d3722edd18c7d986f17fcb
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-28 17:59:39 +0530

    commit

---
+++ L016/Leetcode/uniqChInStr.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    
+    int firstUniqChar(string s) {
+        
+        int n = s.length();
+        
+        vector<int> frequency(26,0);
+        for(int i=0;i<s.length();i++){
+            frequency[s[i] - 'a']+=1;
+        }
+        
+        for(int i=0;i<s.length();i++){
+            if(frequency[s[i] - 'a'] == 1) return i;
+        }
+        return -1;
+    }
+};
