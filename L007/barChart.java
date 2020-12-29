737a6bbbcdeaae7f82f10dd32add87767279b2ef
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-29 22:44:28 +0530

    commit

---
+++ L007/barChart.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+public class barChart {
+
+    public static Scanner sc = new Scanner(System.in);
+    
+    
+    public static void barChart(int[] arr){
+        
+        int max = arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i] > max) max = arr[i]; 
+        }
+        
+        for(int i = max ; i>=1;i--){
+            for(int j=0;j<arr.length;j++){
+                if(arr[j] >= i) System.out.print("*\t");
+                else System.out.print("\t");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        
+        int n = sc.nextInt();
+        
+        int[] arr = new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        barChart(arr);
+    }
+
+}
