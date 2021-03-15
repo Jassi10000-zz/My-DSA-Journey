470d58d81b86ba849d86781984bae6a7d4c3e376
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2021-03-15 19:43:15 +0530

    commit

---
+++ Trees/Binary-Trees/Lec1/Main.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public class Node{
+        int data;
+        Node left = null;
+        Node right = null;
+
+        Node(int data , Node left , Node right){
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
