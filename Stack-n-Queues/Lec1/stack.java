c442a3402e32d1adce525dafa60392f84bdf357d
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2021-03-24 13:16:45 +0530

    completed stack class

--- Stack-n-Queues/Lec1/stack.java
+++ Stack-n-Queues/Lec1/stack.java
@@ -1,55 +1,79 @@
-public class stack{
+public class stack {
     protected int[] arr = null;
     protected int capacity = 0;
     protected int elementCount = 0;
     protected int tos = -1; // tos ---> top of the stack
 
-    //Basic Functions
+    // constructors
 
-    public int size(){
+    protected void initialiseVariables() {
+        this.arr = new int[this.capacity];
+        // we have initialised these two already but here also its very necessary
+        this.elementCount = 0;
+        this.tos = -1;
+
+    }
+
+    // default constructor
+    public stack() {
+        this.capacity = 10;
+
+    }
+
+    // parametrised constructor
+    public stack(int size) {
+        this.capacity = size;
+        initialiseVariables();
+    }
+
+    // Basic Functions
+
+    public int size() {
         return elementCount;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return this.elementCount == 0;
     }
 
-    //Exceptions ----->  1. Overflow Exception ( when stack becomes full and still we are puhsing data )
-    //                   2. Underflow Exception ( when stack is empty and still we are popping data )
+    // Exceptions -----> 1. Overflow Exception ( when stack becomes full and still
+    // we are puhsing data )
+    // 2. Underflow Exception ( when stack is empty and still we are popping data )
 
-    protected void OverflowException() throws Exception{
-        if(this.elementCount == capacity )
+    protected void OverflowException() throws Exception {
+        if (this.elementCount == capacity)
             throw new Exception("Stack is full");
 
     }
-    protected void UnderflowException() throws Exception{
-        if(this.elementCount == 0 ){
+
+    protected void UnderflowException() throws Exception {
+        if (this.elementCount == 0) {
             throw new Exception("Stack is Empty");
         }
     }
 
-    public void push(int data) throws Exception{
+    public void push(int data) throws Exception {
         OverflowException();
         this.arr[++tos] = data;
         this.elementCount++;
     }
 
-    public int top() throws Exception{
+    public int top() throws Exception {
         UnderflowException();
         return this.arr[this.tos];
     }
 
-    protected int pop_(){
-            int returnValue = this.arr[this.tos];
-            this.arr[this.tos--] = 0;
-            this.elementCount--;
-            return returnValue;
+    protected int pop_() {
+        int returnValue = this.arr[this.tos];
+        this.arr[this.tos--] = 0;
+        this.elementCount--;
+        return returnValue;
 
     }
-    public int pop() throws Exception{
+
+    public int pop() throws Exception {
         UnderflowException();
         return pop_();
     }
 
-    
 }
