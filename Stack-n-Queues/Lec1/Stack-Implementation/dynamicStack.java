9f0f0929d96a3c2db883d4f52a02b1f78a09912d
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2021-03-25 20:05:48 +0530

    commit

--- Stack-n-Queues/Lec1/Stack-Implementation/dynamicStack.java
+++ Stack-n-Queues/Lec1/Stack-Implementation/dynamicStack.java
@@ -19,17 +19,16 @@ public class dynamicStack extends stack {
 
     }
 
-    protected void push_(int data) {
+    @Override
+    public void push(int data) {
         if (super.capacity == super.elementCount) {
             int[] temp = super.arr;
             super.initialiseVariables(2 * super.capacity); // agr array full hogya toh size ko double krdo
-            for(int ele : temp) super.push_(ele); // this is not a recursive call , rather a reference to parent's push method
+            for (int ele : temp)
+                super.push_(ele); // this is not a recursive call , rather a reference to parent's push method
 
         }
-        super.push_(data);  // after incrasing the size we now call the parent push method
+        super.push(data); // after incrasing the size we now call the parent push method
     }
 
-    public void push(int data) {
-        push_(data);
-    }
 }
