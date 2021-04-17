3786d5d3e1781b98d1dbdfee1e10e5e3f4d657c2
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2021-04-17 14:13:24 +0530

    started with basics of graph

---
+++ Graph/Lec1/basic.java
@@ -0,0 +1,41 @@
+public class basic{
+
+    public static class Edge{
+        int v = 0;
+        int w = 0;
+        Edge(int v , int w ){
+            this.v = v;
+            this.w = w;
+        }
+    }
+
+    public static int N = 7;
+    public static ArrayList<Edges>[] graph = new ArrayList<>[N];
+
+
+
+    public static addEdge(int u , int v , int w ){
+        
+        //to form a bidirectional graph
+        graph[u].add(new Edge(v,w));
+        graph[v].add(new Edge(v,w));
+
+    }
+
+    public static void main(String[] args){
+
+       for( int u = 0 ; u < N ; u++){
+            graph[i] = new ArrayList<>();
+       }
+       addEdge(0,1,10);
+       addEdge(0,3,10);
+       addEdge(1,2,16);
+       addEdge(2,3,40);
+       addEdge(3,4,2);
+       addEdge(4,5,2);
+       addEdge(4,6,8);
+       addEdge(5,6,3);
+    }
+        
+    }
+}
